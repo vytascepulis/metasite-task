07a57e9209981bafd499f45037be570a134e155c
Vytas <cepulis.vytas@gmail.com>
2025-02-24 12:36:06 +0200

    make main.sass global

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.module.sass";
+import "./main.sass";
 import App from "./containers/App";
 
 createRoot(document.getElementById("root")!).render(
